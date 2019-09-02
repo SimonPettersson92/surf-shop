79c06fc9f49be99d42772fc2738e4b0b47768c8a
Simon Pettersson <simon.pettersson@protonmail.com>
2019-09-02 20:29:14 +0200

    Add user authentication

--- controllers/index.js
+++ controllers/index.js
@@ -2,14 +2,21 @@ const User = require('../models/user')
 
 module.exports = {
 	postRegister(req, res, next) {
-		Account.register(new Account({username: req.body.username}), req.body.password, function(err) {
-    if (err) {
-      console.log('error while user register!', err);
-      return next(err);
-    }
+		const newUser = new User({
+			username: req.body.username,
+			email: req.body.email,
+			image: req.body.image
+
+		});
+		User.register(newUser, req.body.password, (err) => {
+    		if (err) {
+      			console.log('error while user register!', err);
+      			return next(err);
+    		}
 
     console.log('user registered!');
 
     res.redirect('/');
-	}
+	});
+}
 }
