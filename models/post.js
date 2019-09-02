dd18b40957788f0d9300646baf9916436c39a307
Simon Pettersson <simon.pettersson@protonmail.com>
2019-09-02 10:16:47 +0200

    Add models for post, review and user

---
+++ models/post.js
@@ -0,0 +1,11 @@
+/*
+Post
+-title - string
+-price - string
+-description - string
+-images - array of strings
+-location - string
+-lat -number
+-long - number
+-author -object id (ref User)
+-reviews -array of objects
